c7d528bdc417dede075c0fc4a7a19ec3110b1e46
5talyaqw <talyamalka2000@gmail.com>
2026-09-08 16:39:01 +0300

    LAST UPDATES MONDAY

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
+import sys
+import time
 import pygame
+import screen
+import game_field
+import consts
+pygame.init()
+def is_win():
+    # get solider pos:
+    solider_row=game_field.get_solider_row()
+    solider_col=game_field.get_solider_col()
+    if game_field.is_in_flag_location(solider_row,solider_col):
+       screen.draw_win_message()
+       return True
+    return False
+def is_lose():
+
+    #get solider pos:
+    solider_row=game_field.get_solider_feet_row()
+    solider_col=game_field.get_solider_feet_col()
+    if game_field.is_in_bomb_location(solider_row,solider_col):
+        screen.draw_lose_message()
+        return True
+    return False
+def main():
+    run=True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                 pygame.QUIT()
+                 exit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_UP:
+                   # move solider up
+
+                if event.type ==pygame.K_LEFT:
+                    #move solider left
+                if event.key==pygame.K_DOWN:
+                    # move solider down
+                if event.key==pygame.K_RIGHT:
+                    #move solider right
+            if is_lose:
+                time.sleep(2.5)
+                run=False
+
+
+            if is_win:
+              time.sleep(2.5)
+              run = False
+
+
+        pygame.display.update()
+if __name__ == '__main__':
+    main()
--- screen.py
+++ screen.py
@@ -1,1 +1,35 @@
-print("test")
+import consts
+import pygame
+screen = pygame.display.set_mode((100, 100))
+def draw_lose_message():
+    draw_message(consts.LOSE_MESSAGE, consts.LOSE_FONT_SIZE,
+                 consts.LOSE_COLOR, consts.LOSE_LOCATION)
+
+
+def draw_win_message():
+    draw_message(consts.WIN_MESSAGE, consts.WIN_FONT_SIZE,
+                 consts.WIN_COLOR, consts.WIN_LOCATION)
+
+
+def draw_message(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    screen.blit(text_img, location)
+
+import tkinter
+import sys
+import random
+def create_screen():
+    screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
+    pygame.display.set_caption("THE FLAG GAME")
+    screen.fill('lightgreen')
+    bush = pygame.image.load('grass.png')
+    resize_bush = pygame.transform.scale(bush, (60, 60))
+    for i in range (20):
+        ran_x = random.randrange(consts.WINDOW_WIDTH)
+        ran_y = random.randrange(consts.WINDOW_HEIGHT)
+        screen.blit(resize_bush, (ran_x, ran_y))
+    pygame.display.flip()
+    return screen
+
+pygame.display.flip()
